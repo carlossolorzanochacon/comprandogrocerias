156ff048d4403b5cfd69ccaa155c25a9566af3b7
carlossolorzanochacon <carlossolorzano@gmail.com>
2026-08-17 20:15:06 -0600

    Agrega panel visible de control OCR

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_VERSION='comprando-grocerias-v13';
+const CACHE_VERSION='comprando-grocerias-v14';
 const APP_SHELL=[
   './',
   './index.html',
-  './selected-design.css?v=unified-list-9',
+  './selected-design.css?v=unified-list-10',
   './manifest.webmanifest',
   './assets/branding/comprando-grocerias-icon.svg',
   './assets/branding/comprando-grocerias-192.png',
