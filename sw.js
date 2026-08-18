f8d6818b194318f4b655bb5ca4c550ade58e875f
carlossolorzanochacon <carlossolorzano@gmail.com>
2026-08-17 18:42:57 -0600

    Diagnostica correctamente acceso de voz en Android

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_VERSION='comprando-grocerias-v10';
+const CACHE_VERSION='comprando-grocerias-v11';
 const APP_SHELL=[
   './',
   './index.html',
-  './selected-design.css?v=unified-list-6',
+  './selected-design.css?v=unified-list-7',
   './manifest.webmanifest',
   './assets/branding/comprando-grocerias-icon.svg',
   './assets/branding/comprando-grocerias-192.png',
