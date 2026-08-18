9cf722e39fd5ba02c527b0563b5f192e7d9d45d2
carlossolorzanochacon <carlossolorzano@gmail.com>
2026-08-17 21:03:48 -0600

    Ancla editar a la izquierda del comparador

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_VERSION='comprando-grocerias-v20';
+const CACHE_VERSION='comprando-grocerias-v21';
 const APP_SHELL=[
   './',
   './index.html',
-  './selected-design.css?v=unified-list-12',
+  './selected-design.css?v=unified-list-13',
   './manifest.webmanifest',
   './assets/branding/comprando-grocerias-icon.svg',
   './assets/branding/comprando-grocerias-192.png',
